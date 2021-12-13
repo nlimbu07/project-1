0df5fee751c84ca05fe34bffb39152a68d4d945d
Niraj Limbu <nlimbu@yahoo.com>
2021-12-13 18:14:47 -0500

    correted js file

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+// console.log("hello world");
+
+// var apiKey = '5e6b3c6c';
+
+// var onKeyUp = document.getElementById('qurybox').value;
+var answerEl = document.getElementById('answer');
+
 var data;
 
 function getanswer(q) {
-    $.get("http://www.omdbapi.com/?s=" + q + "apikey=5e6b3c6c", function () {
-        var rawstring = JSON.stringify(rawdata);
-        data = JSON.parse(rawstring)
-        var title = data.Search[0].title;
-        var year = data.Search[0].Year;
-        var imbdurl = "https://www.imdb.com/title/" + data.Search[0].imbdID + "/";
+  $.get(
+    'https://www.omdbapi.com/?s=' + q + '&apikey=5e6b3c6c',
+    function (rawdata) {
+      var rawString = JSON.stringify(rawdata);
+      data = JSON.parse(rawString);
+      var titleEl = data.Search[0].Title;
+      var genreEl = data.Search[0].Genre;
+      var imdRating = data.Search[0].Rated;
+      var yearReleased = data.Search[0].Year;
+      var directorEl = data.Search[0].Director;
+      var imdbUrl = 'https://www.imdb.com/title/' + data.Search[0].imdbID + '/';
 
-        document.getElementById('answer').innerHTML = "<h1>" + title + "</h1><p>Year Released: " + year + "</p><br><p>IMDb page: <a href=" + imbdurl + "target='_blank'>" + imbdul + "</a></p>";
-    })
+      var posterUrl = data.Search[0].Poster;
+
+      answerEl.innerHTML = `
+      <h1>${titleEl}</h1><br>
+      <img src= '${posterUrl}'><br>
+      <p>Genre: ${genreEl}</p><br>
+      <p> Year Released:${yearReleased}</p> <br>
+      <p>Director: ${directorEl}</p><br>
+      <p> IMDb Rating: ${imdRating}</p><br>
+        <p> IMDB page: <a href='${imdbUrl}'target='_blank'>${imdbUrl}</a></p>
+        `;
+    }
+  );
 }
+
+$('#search').on('click', getanswer);
