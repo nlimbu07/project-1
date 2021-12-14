88b8591706f2c8601f643482c574bc0246702b5c
Niraj Limbu <nlimbu@yahoo.com>
2021-12-13 19:43:01 -0500

    update js file

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 var apiKey = '5e6b3c6c';
 
+var answerEl = document.querySelector('#answer');
+
 var data;
 
 function getanswer(q) {
@@ -11,6 +13,7 @@ function getanswer(q) {
     console.log(data);
     var titleEl = data.Title;
     var genreEl = data.Genre;
+    var actors = data.Actors;
     var imdRating = data.Rated;
     var yearReleased = data.Year;
     var directorEl = data.Director;
@@ -23,6 +26,7 @@ function getanswer(q) {
       <h1>${titleEl}</h1><br>
       <img src= '${posterUrl}'><br>
       <p>Genre: ${genreEl}</p><br>
+      <p>Actors: ${actors}</p><br>
       <p> Year Released:${yearReleased}</p> <br>
       <p>Director: ${directorEl}</p><br>
       <p> IMDb Rating: ${imdRating}</p><br>
