01efd05bf076f985902b5446cb782ec42dc5bf97
ingramwd <Ingramwd01@gmail.com>
2021-12-14 17:36:16 -0500

    editing variables to be more logical

--- script.js
+++ script.js
@@ -18,21 +18,30 @@ function getanswer(q) {
         var yearReleased = data.Year;
         var directorEl = data.Director;
         var posterid = data.imdbID
-        var imdbUrl = 'https://www.imdb.com/title/' + data.imdbID + '/';
+        var imdbApi = 'https://www.imdb.com/title/' + data.imdbID + '/';
 
         // Postor api is not working
-        var posterUrl = 'http://img.omdbapi.com/?i=' + posterid + '&h=600&apikey=5e6b3c6c';
+        var posterApi = 'http://img.omdbapi.com/?i=' + posterid + '&h=600&apikey=5e6b3c6c';
+
+        console.log('posterApi', posterApi)
+
+        $.get(posterApi, function () {
+            answerEl.innerHTML = `
+            <h1>${titleEl}</h1><br>
+            <img src= '${posterApi}'><br>
+            <p>Genre: ${genreEl}</p><br>
+            <p>Actors: ${actors}</p><br>
+            <p> Year Released:${yearReleased}</p> <br>
+            <p>Director: ${directorEl}</p><br>
+            <p> IMDb Rating: ${imdRating}</p><br>
+              <p> IMDB page: <a href='${imdbApi}'target='_blank'>${imdbUrl}</a></p>
+              `;
+        });
 
 
-        answerEl.innerHTML = `
-      <h1>${titleEl}</h1><br>
-      <img src= '${posterUrl}'><br>
-      <p>Genre: ${genreEl}</p><br>
-      <p>Actors: ${actors}</p><br>
-      <p> Year Released:${yearReleased}</p> <br>
-      <p>Director: ${directorEl}</p><br>
-      <p> IMDb Rating: ${imdRating}</p><br>
-        <p> IMDB page: <a href='${imdbUrl}'target='_blank'>${imdbUrl}</a></p>
-        `;
     });
+}
+
+function localStorage(movie) {
+
 }
