f5e6c1503563180f2059240b5b3c8a0c892e06fb
Niraj Limbu <nlimbu@yahoo.com>
2021-12-15 18:25:09 -0500

    ready to push to main

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ var movieInfoEl = document.querySelector('#movie-info');
 var data;
 
 function getMovieInfo(info) {
-<<<<<<< HEAD
   //This url is used to get the data from the server side and then we declare
   // variables and set them to the data we want from the server.
   $.get(
@@ -17,6 +16,7 @@ function getMovieInfo(info) {
       var rawString = JSON.stringify(rawdata);
       data = JSON.parse(rawString);
       console.log(data.imdbID);
+      var title = data.Title;
       var genreEl = data.Genre;
       var actors = data.Actors;
       var rated = data.Rated;
@@ -44,8 +44,11 @@ function getMovieInfo(info) {
         // sets all of the retrieved data into dynamically created elements within the
         // movieInfoEl
         movieInfoEl.innerHTML = `
+        
             <img class="image" src= '${posterApi}'><br><br>
-            <p>Genre: <i style="color: orange;">${genreEl}</i><br><br>
+            
+            <p>Movie Name: <i style="color: orange;">${title}</i><br><br>
+            Genre: <i style="color: orange;">${genreEl}</i><br><br>
             Rated: <i style="color: orange;">${rated}</i><br><br>            
             Released: <i style="color: orange;">${yearReleased}</i><br><br>
             Runtime: <i style="color: orange;">${runtime}</i><br><br>
@@ -60,57 +63,4 @@ function getMovieInfo(info) {
       });
     }
   );
-=======
-    //This url is used to get the data from the server side and then we declare 
-    // variables and set them to the data we want from the server.
-    $.get(`https://www.omdbapi.com/?t=${info}&apikey=${apiKey}`, function (rawdata) {
-        var rawString = JSON.stringify(rawdata);
-        data = JSON.parse(rawString);
-        console.log(data.imdbID);
-        var titleEl = data.Title;
-        var genreEl = data.Genre;
-        var actors = data.Actors;
-        var imdRating = data.Rated;
-        var yearReleased = data.Year;
-        var directorEl = data.Director;
-        var posterid = data.imdbID
-        var imdbUrl = 'https://www.imdb.com/title/' + data.imdbID + '/';
-
-        // This is our second Api to retrieve our movie poster img
-        var posterApi = 'http://img.omdbapi.com/?i=' + posterid + '&h=600&apikey=5e6b3c6c';
-
-        // console logging to test Poster Api response
-        // because not all movies have a poster available
-        console.log('posterApi', posterApi)
-
-        // This function prevents a movie without a poster 
-        // from displaying onto the webpage
-        $.get(posterApi, function () {
-            // sets all of the retrieved data into dynamically created elements within the 
-            // movieInfoEl
-            movieInfoEl.innerHTML = `
-            <div class='columns'>
-            <div class='column'>
-            <h1>Movie Title: ${titleEl}</h1><br>
-            </div>
-            <div class='poster column is half'>
-            <img src= '${posterApi}'><br>
-            </div>
-            <div class='column'>
-            <div class='columns'>
-            <div class='column'>
-            <p>Genre: ${genreEl}</p><br>
-            <p>Actors: ${actors}</p><br>
-            <p> Year Released:${yearReleased}</p> <br>
-            <p>Director: ${directorEl}</p><br>
-            <p> IMDb Rating: ${imdRating}</p><br>
-            <p> IMDB page: <a class='anchor' href='${imdbUrl}'target='_blank'>${imdbUrl}</a></p>
-            </div>
-            </div>  
-              `;
-        });
-
-
-    });
->>>>>>> 4f35466ccce6f412e9ddba1bad414b01d9139c15
 }
